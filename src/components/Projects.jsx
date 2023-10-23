38834e4b45b486a9876fb43e68106247933daa69
anas ismail <msanasismail@gmail.com>
2023-10-23 22:13:33 +0530

    project section animation added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
 import { info } from "../info/Info";
+import { motion,useScroll, useTransform } from "framer-motion";
 
 const Projects = () => {
+
+    const ref = useRef(null)
+    const {scrollYProgress} = useScroll({
+      target: ref,
+      offset : ["0 1","0.8 1"]
+    })
+  
+  const scaleProgress = useTransform(scrollYProgress,[0 , 1] , [0.5, 1])
+
   return (
-    <div className="w-screen bg-black h-fit flex flex-col items-center justify-center md:p-12">
+    <motion.div style={{scale: scaleProgress , opacity: scrollYProgress}} ref={ref} className="w-screen bg-black h-fit flex flex-col items-center justify-center md:p-12">
         <h1 className="text-gray font-bold text-3xl tracking-wide mt-12 md:mt-0">PROJECTS</h1>
       <section className="w-4/5 md:w-3/4 mt-10 md:p-8 bord flex flex-col md:flex-row items-center justify-center ">
         <section className="w-full md:w-2/5 rounded-md bg-[#1F1F1F] hover:animate-projectHover hover:origin-[70%_70%]">
           <img className="" src={info.template} alt="template" />
           <section className="p-6 text-white">
-            <h3 className="text-2xl font-semibold tracking-wide">
+            <h3 className="text-2xl text-[#EBECF0] font-semibold tracking-wider">
               PROJECT ONE
             </h3>
             <section className="mt-4 flex items-start">
@@ -25,7 +35,7 @@ const Projects = () => {
         <section className="w-full md:w-2/5 mt-12 md:mt-0 md:ml-20 rounded-md bg-[#1F1F1F] hover:animate-projectHover hover:origin-[70%_70%]">
           <img className="" src={info.template} alt="template" />
           <section className="p-6 text-white">
-            <h3 className="text-2xl font-semibold tracking-wide">
+            <h3 className="text-2xl text-[#EBECF0] font-semibold tracking-wider">
               PROJECT ONE
             </h3>
             <section className="mt-4 flex items-start">
@@ -44,7 +54,7 @@ const Projects = () => {
         <section className="w-full md:w-2/5 mt-12 md:mt-0 rounded-md bg-[#1F1F1F] hover:animate-projectHover hover:origin-[70%_70%]">
           <img className="" src={info.template} alt="template" />
           <section className="p-6 text-white">
-            <h3 className="text-2xl font-semibold tracking-wide">
+            <h3 className="text-2xl text-[#EBECF0] font-semibold tracking-wider">
               PROJECT ONE
             </h3>
             <section className="mt-4 flex items-start">
@@ -60,7 +70,7 @@ const Projects = () => {
         <section className="w-full md:w-2/5 mt-12 md:mt-0 md:ml-20 rounded-md bg-[#1F1F1F] hover:animate-projectHover hover:origin-[70%_70%]">
           <img className="" src={info.template} alt="template" />
           <section className="p-6 text-white">
-            <h3 className="text-2xl font-semibold tracking-wide">
+            <h3 className="text-2xl text-[#EBECF0] font-semibold tracking-wider">
               PROJECT ONE
             </h3>
             <section className="mt-4 flex items-start">
@@ -74,7 +84,7 @@ const Projects = () => {
           </section>
         </section>
       </section>
-    </div>
+    </motion.div>
   );
 };
 
