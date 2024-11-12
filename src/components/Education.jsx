62979b10f49e4bca6f7d562787333cedb8ad5ccf
anas ismail <msanasismail@gmail.com>
2024-11-12 12:49:48 +0530

    info

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -19,17 +19,17 @@ const Education = () => {
       <div className="border-bw bg-[#1F1F1F] rounded-md p-4 md:p-8 md:pl-12 border-l-8 border-l-baseColor flex flex-col items-left justify-center w-4/5 md:w-3/4 ">
         <div>
           <h1 className="text-gray font-bold text-3xl tracking-wide">
-            Education
+            Experience
           </h1>
           <h2 className="text-2xl text-white font-semibold mt-8">
-            BE, Electronics and Communication Engineering
+            Web Developer Intern
           </h2>
           <p className="text-lg text-[#6c757d] font-normal mt-8">
-            Adhiyamaan College of Engineering
+            Talrop, Kerala
           </p>
-          <p className="text-lg text-[#6c757d] font-normal mt-8">2020 - 2024</p>
+          <p className="text-lg text-[#6c757d] font-normal mt-8">Present</p>
         </div>
-        <div>
+        <div className="mt-6">
           <h1 className="text-gray font-bold text-3xl tracking-wide">
             Education
           </h1>
