be67f096ce7bc85671583bc810be665c459cd3f0
anas ismail <msanasismail@gmail.com>
2024-11-12 11:15:42 +0530

    grog

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,23 +1,28 @@
 import React, { useRef } from "react";
 import { info } from "../info/Info";
-import { motion,useScroll, useTransform } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const Projects = () => {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "0.8 1"],
+  });
 
-    const ref = useRef(null)
-    const {scrollYProgress} = useScroll({
-      target: ref,
-      offset : ["0 1","0.8 1"]
-    })
-  
-  const scaleProgress = useTransform(scrollYProgress,[0 , 1] , [0.9, 1])
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.9, 1]);
 
   const alertMsg = () => {
-    alert("This Repositery is private")
-  }
+    alert("This Repositery is private");
+  };
   return (
-    <motion.div style={{scale: scaleProgress , opacity: scrollYProgress}} ref={ref} className="w-screen bg-black h-fit flex flex-col items-center justify-center md:p-12">
-        <h1 className="text-gray font-bold text-3xl tracking-wide mt-12 md:mt-0">PROJECTS</h1>
+    <motion.div
+      style={{ scale: scaleProgress, opacity: scrollYProgress }}
+      ref={ref}
+      className="w-screen bg-black h-fit flex flex-col items-center justify-center md:p-12"
+    >
+      <h1 className="text-gray font-bold text-3xl tracking-wide mt-12 md:mt-0">
+        PROJECTS
+      </h1>
       <section className="w-4/5 md:w-3/4 mt-10 md:p-8 bord flex flex-col md:flex-row items-center justify-center ">
         <section className="w-full md:w-2/5 rounded-md bg-[#1F1F1F] hover:animate-projectHover hover:origin-[70%_70%]">
           <img className="" src={info.template} alt="template" />
@@ -27,27 +32,42 @@ const Projects = () => {
             </h3>
             <section className="mt-4 flex items-start">
               <button className="rounded-md border-2 border-[#70F6C0] hover:border-white pt-pad pb-pad pl-3 pr-3 hover:text-baseColor transition-all">
-                <a target="_blank" href="https://talrop-euphoria.netlify.app/">Live</a>
+                <a target="_blank" href="https://talrop-euphoria.netlify.app/">
+                  Live
+                </a>
               </button>
               <button className="ml-10 rounded-md border-2 border-[#70F6C0] hover:border-white pt-pad pb-pad pl-3 pr-3 hover:text-baseColor transition-all">
-                <a target="_blank" href="https://github.com/anasis22/talrop-euphoria">Code</a>
+                <a
+                  target="_blank"
+                  href="https://github.com/anasis22/talrop-euphoria"
+                >
+                  Code
+                </a>
               </button>
             </section>
           </section>
         </section>
         <section className="p-6 text-white">
-            <h3 className="text-xl md:2xl text-[#EBECF0] font-semibold tracking-wider">
-              Grogin Grocery
-            </h3>
-            <section className="mt-4 flex items-start">
-              <button className="rounded-md border-2 border-[#70F6C0] hover:border-white pt-pad pb-pad pl-3 pr-3 hover:text-baseColor transition-all">
-                <a target="_blank" href="https://talrop-grogin.netlify.app/">Live</a>
-              </button>
-              <button className="ml-10 rounded-md border-2 border-[#70F6C0] hover:border-white pt-pad pb-pad pl-3 pr-3 hover:text-baseColor transition-all">
-                <a target="_blank" href="https://github.com/anasis22/talrop-grogin">Code</a>
-              </button>
-            </section>
+          <img className="" src={info.template} alt="template" />
+          <h3 className="text-xl md:2xl text-[#EBECF0] font-semibold tracking-wider">
+            Grogin Grocery
+          </h3>
+          <section className="mt-4 flex items-start">
+            <button className="rounded-md border-2 border-[#70F6C0] hover:border-white pt-pad pb-pad pl-3 pr-3 hover:text-baseColor transition-all">
+              <a target="_blank" href="https://talrop-grogin.netlify.app/">
+                Live
+              </a>
+            </button>
+            <button className="ml-10 rounded-md border-2 border-[#70F6C0] hover:border-white pt-pad pb-pad pl-3 pr-3 hover:text-baseColor transition-all">
+              <a
+                target="_blank"
+                href="https://github.com/anasis22/talrop-grogin"
+              >
+                Code
+              </a>
+            </button>
           </section>
+        </section>
       </section>
       {/* second section */}
       <section className="w-4/5 md:w-3/4 mt-10 md:p-8 bord flex flex-col md:flex-row items-center justify-center ">
@@ -59,10 +79,17 @@ const Projects = () => {
             </h3>
             <section className="mt-4 flex items-start">
               <button className="rounded-md border-2 border-[#70F6C0] hover:border-white pt-pad pb-pad pl-3 pr-3 hover:text-baseColor transition-all">
-                <a target="_blank" href="https://talrop-grogin.netlify.app/">Live</a>
+                <a target="_blank" href="https://talrop-grogin.netlify.app/">
+                  Live
+                </a>
               </button>
               <button className="ml-10 rounded-md border-2 border-[#70F6C0] hover:border-white pt-pad pb-pad pl-3 pr-3 hover:text-baseColor transition-all">
-                <a target="_blank" href="https://github.com/anasis22/talrop-grogin">Code</a>
+                <a
+                  target="_blank"
+                  href="https://github.com/anasis22/talrop-grogin"
+                >
+                  Code
+                </a>
               </button>
             </section>
           </section>
@@ -75,10 +102,20 @@ const Projects = () => {
             </h3>
             <section className="mt-4 flex items-start">
               <button className="rounded-md border-2 border-[#70F6C0] hover:border-white pt-pad pb-pad pl-3 pr-3 hover:text-baseColor transition-all">
-                <a target="_blank" href="https://anasis22.github.io/Task-tracker/">Live</a>
+                <a
+                  target="_blank"
+                  href="https://anasis22.github.io/Task-tracker/"
+                >
+                  Live
+                </a>
               </button>
               <button className="ml-10 rounded-md border-2 border-[#70F6C0] hover:border-white pt-pad pb-pad pl-3 pr-3 hover:text-baseColor transition-all">
-                <a target="_blank" href="https://github.com/anasis22/Task-tracker">Code</a>
+                <a
+                  target="_blank"
+                  href="https://github.com/anasis22/Task-tracker"
+                >
+                  Code
+                </a>
               </button>
             </section>
           </section>
@@ -94,10 +131,20 @@ const Projects = () => {
             </h3>
             <section className="mt-4 flex items-start">
               <button className="rounded-md border-2 border-[#70F6C0] hover:border-white pt-pad pb-pad pl-3 pr-3 hover:text-baseColor transition-all">
-                <a target="_blank" href="https://anasis22.github.io/Weather-Info/">Live</a>
+                <a
+                  target="_blank"
+                  href="https://anasis22.github.io/Weather-Info/"
+                >
+                  Live
+                </a>
               </button>
               <button className="ml-10 rounded-md border-2 border-[#70F6C0] hover:border-white pt-pad pb-pad pl-3 pr-3 hover:text-baseColor transition-all">
-                <a target="_blank" href="https://github.com/anasis22/Weather-Info">Code</a>
+                <a
+                  target="_blank"
+                  href="https://github.com/anasis22/Weather-Info"
+                >
+                  Code
+                </a>
               </button>
             </section>
           </section>
@@ -110,10 +157,14 @@ const Projects = () => {
             </h3>
             <section className="mt-4 flex items-start">
               <button className="rounded-md border-2 border-[#70F6C0] hover:border-white pt-pad pb-pad pl-3 pr-3 hover:text-baseColor transition-all">
-                <a target="_blank" href="https://ece-students-info.web.app/">Live</a>
+                <a target="_blank" href="https://ece-students-info.web.app/">
+                  Live
+                </a>
               </button>
               <button className="ml-10 rounded-md border-2 border-[#70F6C0] hover:border-white pt-pad pb-pad pl-3 pr-3 hover:text-baseColor transition-all">
-                <a target="_blank" onClick={alertMsg}>Code</a>
+                <a target="_blank" onClick={alertMsg}>
+                  Code
+                </a>
               </button>
             </section>
           </section>
@@ -129,10 +180,20 @@ const Projects = () => {
             </h3>
             <section className="mt-4 flex items-start">
               <button className="rounded-md border-2 border-[#70F6C0] hover:border-white pt-pad pb-pad pl-3 pr-3 hover:text-baseColor transition-all">
-                <a target="_blank" href="https://anasis22.github.io/bulk-mailer/">Live</a>
+                <a
+                  target="_blank"
+                  href="https://anasis22.github.io/bulk-mailer/"
+                >
+                  Live
+                </a>
               </button>
               <button className="ml-10 rounded-md border-2 border-[#70F6C0] hover:border-white pt-pad pb-pad pl-3 pr-3 hover:text-baseColor transition-all">
-                <a target="_blank" href="https://github.com/anasis22/bulk-mailer">Code</a>
+                <a
+                  target="_blank"
+                  href="https://github.com/anasis22/bulk-mailer"
+                >
+                  Code
+                </a>
               </button>
             </section>
           </section>
@@ -145,10 +206,14 @@ const Projects = () => {
             </h3>
             <section className="mt-4 flex items-start">
               <button className="rounded-md border-2 border-[#70F6C0] hover:border-white pt-pad pb-pad pl-3 pr-3 hover:text-baseColor transition-all">
-                <a target="_blank" href="https://anasis22.github.io/bmi-calc/">Live</a>
+                <a target="_blank" href="https://anasis22.github.io/bmi-calc/">
+                  Live
+                </a>
               </button>
               <button className="ml-10 rounded-md border-2 border-[#70F6C0] hover:border-white pt-pad pb-pad pl-3 pr-3 hover:text-baseColor transition-all">
-                <a target="_blank" href="https://github.com/anasis22/bmi-calc">Code</a>
+                <a target="_blank" href="https://github.com/anasis22/bmi-calc">
+                  Code
+                </a>
               </button>
             </section>
           </section>
